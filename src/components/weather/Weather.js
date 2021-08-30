5722180f32919bc1cce7dc21a7c9f270f46d944a
Aden <adeziio@yahoo.com>
2021-08-30 01:32:42 -0400

    add env for weather api service

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
+import { Input } from 'semantic-ui-react';
 
 export default class Weather extends Component {
     constructor(props) {
         super(props);
         this.state = {
+
         }
     }
 
     componentDidMount() {
     }
 
+    fetchData = (search) => {
+        fetch("https://weatherapi-com.p.rapidapi.com/forecast.json?q=" + search, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+                "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY
+            }
+        })
+            .then(response => {
+                console.log(response);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+
     render() {
+
         return (
-            <div>Weather</div>
+            <div>
+                <Input placeholder='Search City or Zip Code' />
+            </div>
         )
     }
 }
